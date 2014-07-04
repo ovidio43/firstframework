2e371e82f58dae066887426121c8c0120d430837
Aldo Adachi <aldo.adachi@iconicweb.com>
2014-07-04 15:33:25 -0400

    edd

--- helloworld.php
+++ helloworld.php
@@ -1,7 +1,12 @@
 <?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
 $confidencial="esto es confidencial";
 $languaje = "PHP";
+// declarando una funcion
 function view($languaje){
 	require "view.php";	
 }
+//llamando una funcion
 view($languaje);
